6efde2e77559ee56dd28e1e59c3ab401efee0842
Grzegorz <g.w.mika@gmail.com>
2021-05-01 21:38:26 +0200

    move to cloud

--- collect_data/api_call.py
+++ collect_data/api_call.py
@@ -2,10 +2,11 @@ import sched
 import time
 
 import requests
-from pymongo import MongoClient
+# from pymongo import MongoClient
+from google.cloud import firestore
 
 s = sched.scheduler(time.time, time.sleep)
-delay = 5
+delay = 900
 params = {
     "appid": "3f096ec5f65dc99c42e1a8c5b785826f",
     "q": "Kraków",
@@ -13,16 +14,19 @@ params = {
     "units": "metric"
 }
 
-client = MongoClient('mongodb://127.0.0.1:27017')
-db = client.pogoda
+# client = MongoClient('mongodb://127.0.0.1:27017')
+# db = client.pogoda
+db = firestore.Client()
+collection = db.collection(u'raw_data')
 
 
 def run_calls():
     try:
         response = requests.get(
             "http://api.openweathermap.org/data/2.5/weather", params=params)
-        result = db.raw_data.insert_one(response.json())
-        print(result.inserted_id)
+        # result = db.raw_data.insert_one(response.json())
+        # print(result.inserted_id)
+        collection.add(response.json())
     except ConnectionError as e:
         print(e)
     s.enter(delay, 1, run_calls)
