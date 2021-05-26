d42ae61cdaac65ed15a2dd95fbb78d66d6835275
Grzegorz <g.w.mika@gmail.com>
2021-05-26 23:17:32 +0200

    connect and write data to bigquery

--- process_data/data_processor.py
+++ process_data/data_processor.py
@@ -1,7 +1,7 @@
+import logging
 from datetime import datetime
 
-from google.cloud import firestore
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 
 
 class DataProcessor:
@@ -20,95 +20,149 @@ class DataProcessor:
         }
         self.bigquery = create_engine(
             'bigquery://pogoda-312419/pogoda',
-            credentials_path=credentials_path
+            credentials_path=credentials_path,
+            echo=True
         )
-        self.firestore = firestore.Client()
+        m = MetaData()
+        m.reflect(bind=self.bigquery)
+        self.tables = m.tables
 
     def __call__(self, response):
-        for key, function in self.methods:
+        for key, function in self.methods.items():
             self.insert_data(response, key, function)
 
     def insert_data(self, response, key, function):
-        pass
+        data = function(response)
+        identifier = self.get_key(response)
+        to_insert = identifier + data
+        table = self.tables[key]
+        ins = table.insert().values(to_insert)
+        with self.bigquery.connect() as connection:
+            result = connection.execute(ins)
+        logging.info(result)
 
     @staticmethod
     def get_temperature_info(response):
-        return [
-            response.get('main').get('temp'),
-            response.get('main').get('feels_like'),
-            response.get('main').get('temp_min'),
-            response.get('main').get('temp_max')
-        ]
+        try:
+            return [
+                response.get('main').get('temp'),
+                response.get('main').get('feels_like'),
+                response.get('main').get('temp_min'),
+                response.get('main').get('temp_max')
+            ]
+        except AttributeError as e:
+            logging.error(e)
+            return [None, None, None, None]
 
     @staticmethod
     def get_pressure_info(response):
-        return [
-            response.get('main').get('pressure'),
-            response.get('main').get('sea_level'),
-            response.get('main').get('grnd_level')
-        ]
+        try:
+            return [
+                response.get('main').get('pressure'),
+                response.get('main').get('sea_level'),
+                response.get('main').get('grnd_level')
+            ]
+        except AttributeError as e:
+            logging.error(e)
+            return [None, None, None]
 
     @staticmethod
     def get_humidity_info(response):
-        return response.get('main').get('humidity')
+        try:
+            return [response.get('main').get('humidity')]
+        except AttributeError as e:
+            logging.error(e)
+            return [None]
 
     @staticmethod
     def get_wind_info(response):
-        return [
-            response.get('wind').get('speed'),
-            response.get('wind').get('deg'),
-            response.get('wind').get('gust')
-        ]
+        try:
+            return [
+                response.get('wind').get('speed'),
+                response.get('wind').get('deg'),
+                response.get('wind').get('gust')
+            ]
+        except AttributeError as e:
+            logging.error(e)
+            return [None, None, None]
 
     @staticmethod
     def get_clouds_info(response):
-        return [
-            response.get('clouds').get('all'),
-            response.get('visibility')
-        ]
+        try:
+            return [
+                response.get('clouds').get('all'),
+                response.get('visibility')
+            ]
+        except AttributeError as e:
+            logging.error(e)
+            return [None, None]
 
     @staticmethod
     def get_rain_info(response):
-        return [
-            response.get('rain').get('1h'),
-            response.get('rain').get('3h')
-        ]
+        try:
+            return [
+                response.get('rain').get('1h'),
+                response.get('rain').get('3h')
+            ]
+        except AttributeError as e:
+            logging.error(e)
+            return [None, None]
 
     @staticmethod
     def get_snow_info(response):
-        return [
-            response.get('snow').get('1h'),
-            response.get('snow').get('3h')
-        ]
+        try:
+            return [
+                response.get('snow').get('1h'),
+                response.get('snow').get('3h')
+            ]
+        except AttributeError as e:
+            logging.error(e)
+            return [None, None]
 
     @staticmethod
     def get_sun_info(response):
-        return [
-            response.get('sys').get('sunrise'),
-            response.get('sys').get('sunset')
-        ]
+        try:
+            return [
+                datetime.utcfromtimestamp(response.get('sys').get('sunrise') + response.get('timezone')).strftime("%Y-%m-%d %H:%M:%S"),
+                datetime.utcfromtimestamp(response.get('sys').get('sunset') + response.get('timezone')).strftime("%Y-%m-%d %H:%M:%S")
+            ]
+        except AttributeError as e:
+            logging.error(e)
+            return [None, None]
 
     @staticmethod
     def get_location_info(response):
-        return [
-            response.get('name'),
-            response.get('sys').get('country'),
-            response.get('coord').get('lon'),
-            response.get('coord').get('lat')
-        ]
+        try:
+            return [
+                response.get('name'),
+                response.get('sys').get('country'),
+                response.get('coord').get('lon'),
+                response.get('coord').get('lat')
+            ]
+        except AttributeError as e:
+            logging.error(e)
+            return [None, None, None, None]
 
     @staticmethod
     def get_weather_info(response):
-        return [
-            response.get('weather')[0].get('id'),
-            response.get('weather')[0].get('main'),
-            response.get('weather')[0].get('description'),
-            response.get('weather')[0].get('icon')
-        ]
+        try:
+            return [
+                response.get('weather')[0].get('id'),
+                response.get('weather')[0].get('main'),
+                response.get('weather')[0].get('description'),
+                response.get('weather')[0].get('icon')
+            ]
+        except AttributeError as e:
+            logging.error(e)
+            return [None, None, None, None]
 
     @staticmethod
     def get_key(response):
-        return [
-            response.get('id'),
-            datetime.utcfromtimestamp(response.get('dt') + response.get('timezone')).strftime("%Y-%m-%d %H:%M:%S")
-        ]
+        try:
+            return [
+                response.get('id'),
+                datetime.utcfromtimestamp(response.get('dt') + response.get('timezone')).strftime("%Y-%m-%d %H:%M:%S")
+            ]
+        except AttributeError as e:
+            logging.error(e)
+            return [None, None]
