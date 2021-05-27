5377ee9e8c5e5f13895b3a64e9de108afb57ea71
Grzegorz <g.w.mika@gmail.com>
2021-05-27 20:39:06 +0200

    env var

--- process_data/load.py
+++ process_data/load.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 from google.cloud import firestore
@@ -5,17 +6,18 @@ from tqdm import tqdm
 
 from data_processor import DataProcessor
 
-db = firestore.Client()
-collection = db.collection(u'raw_data')
-
 if __name__ == '__main':
     credentials_path = sys.argv[1]
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credentials_path
+
     data_processor = DataProcessor(credentials_path)
 
     with data_processor.bigquery.connect() as conn:
         res = conn.execute('SELECT max(dt) AS last_dt FROM time;')
     last_dt = res.fetchone()['last_dt']
 
+    db = firestore.Client()
+    collection = db.collection(u'raw_data')
     docs = collection.where(u'dt', u'>', last_dt).stream()
 
     for doc in tqdm(docs):
